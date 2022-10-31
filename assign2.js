e7e09ab338beff543fd4735e20687cd60eb48295
Daniel <danielbram4@gmail.com>
2022-10-30 18:01:39 -0600

    first push

---
+++ assign2.js
@@ -0,0 +1,46 @@
+/* url of song api --- https versions hopefully a little later this semester */	
+const api = 'http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
+
+
+/* note: you may get a CORS error if you try fetching this locally (i.e., directly from a
+   local file). To work correctly, this needs to be tested on a local web server.  
+   Some possibilities: if using Visual Code, use Live Server extension; if Brackets,
+   use built-in Live Preview.
+*/
+
+
+const creditButton = document.querySelector("#creditButton");
+function creditButtonHandler()
+{
+   let myDropdown = document.querySelector("#myDropdown");
+   myDropdown.classList.remove("dropDown-hidden");
+   myDropdown.classList.add("dropdown-content")
+}
+
+creditButton.addEventListener("click", creditButtonHandler);
+
+const clearButton = document.querySelector("#clearButton");
+function clearButtonHandler()
+{
+   alert("Cleared!")
+}
+
+clearButton.addEventListener("click", clearButtonHandler);
+
+const searchButton = document.querySelector("#searchButton");
+function searchButtonHandler()
+{
+   alert("Searched!")
+}
+
+searchButton.addEventListener("click", searchButtonHandler);
+
+const addButton = document.querySelector("#addButton");
+function addButtonHandler()
+{
+   let myDropdown = document.querySelector("#addBtn #myDropdown");
+   myDropdown.classList.remove("dropDown-hidden");
+   myDropdown.classList.add("dropdown-content")
+}
+
+addButton.addEventListener("click", addButtonHandler);
